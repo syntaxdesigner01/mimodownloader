4e8b9bcb63084f4630724b7c98388594c0256e1d
syntaxdesigner01 <148327008+syntaxdesigner01@users.noreply.github.com>
2023-10-18 16:56:21 +0100

    Development (#6)
    * created chakar ui provider
    
    * done creating the home page
    
    * added navabar
    done working on the home page
    
    * fixed
    
    * worked on the mp3 page
    
    * added mp3 file
    
    * fixed
    
    * added loader
    
    * added loader
    
    * fixed
    
    ---------
    
    Co-authored-by: akpan joseph <akpanjoseph2021@gmail..com>

--- src/app/Pages/DownloadMp3/page.tsx
+++ src/app/Pages/DownloadMp3/page.tsx
@@ -1,21 +1,42 @@
 "use client"
 
-import { Input, Box, Flex, Text, Heading } from "@chakra-ui/react"
+import { SearchIcon } from "@chakra-ui/icons"
+import { Input, Box, Flex, Text, Heading, Spinner } from "@chakra-ui/react"
+
 
 
 
 export default function page() {
   return (
-    <div className="flex mt-[5%] w-full items-center justify-center">
+    <div className="flex flex-col mt-[5%] w-full items-center justify-center">
 
       <Box w={'full'}>
         <Heading textAlign={'center'} my={10}>Youtube To Mp3</Heading>
 
-        <Flex w={'full'} justifyContent={'center'} flexDirection={{ base: 'column', md: 'row' }}>
-          <Input type="text" w={{ base: 'full', md: '40%' }} border={'2px'} />
-          <button type="submit" className=" bg-orange-600 px-4 mx-2 rounded-sm">Search</button>
+        <Flex w={'full'} alignItems={'center'} justifyContent={'center'} gap={{ base: 4, md: 1 }} flexDirection={{ base: 'column', md: 'row' }}>
+          <Input type="text" px={'10px'} w={{ base: '90%', md: '40%' }} border={'2px'} placeholder="Paste link here.." />
+          <button type="submit" className=" w-[70%] md:w-[10%] font-bold py-2 bg-orange-600 px-4 mx-2 rounded-md hover:text-white">
+            <Flex alignItems={'center'} justifyContent={'center'} gap={1}>
+            <SearchIcon fontSize={20} /> <Text>Search</Text>
+            </Flex>
+          </button>
         </Flex>
 
+       
+      </Box>
+
+      <Box  mt={10}>
+      <Flex justifyContent={'center'} alignItems={'center'} gap={4}>
+        <Spinner
+          thickness='4px'
+          speed='0.65s'
+          emptyColor='black'
+          color='orange.500'
+          size='xl'
+          p={4}
+        />
+        <Text>Please wait..</Text>
+      </Flex>
       </Box>
 
     </div>
