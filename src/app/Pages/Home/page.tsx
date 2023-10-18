d03acdd710d23951d425eec78802df20ebc76849
akpan joseph <akpanjoseph2021@gmail..com>
2023-10-18 13:48:22 +0100

    fixed

---
+++ src/app/Pages/Home/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/Header"
+import Footer from "@/app/components/footer"
+import NavBar from "@/app/components/navbar"
+
+function HomePage() {
+  return (
+    <div>
+            <Header/>
+            <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
