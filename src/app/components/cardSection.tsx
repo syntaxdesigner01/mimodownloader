dc3815c746300bfbb7421f728cdbc247d341d63a
akpan joseph <syntaxdesigners@gmail.com>
2023-10-19 16:22:45 +0100

    edited ui

--- src/app/components/cardSection.tsx
+++ src/app/components/cardSection.tsx
@@ -11,25 +11,28 @@ export default function CardSection() {
             overflow='hidden'
             // variant='filled'
             px={{ base: 10, md: 0 }}
+            border={'1px'}
+            borderColor={'gray.100'}
+            boxShadow={'2xl'}
+            rounded={'2xl'}
         >
-            <Image
+            {/* <Image
                 objectFit='cover'
-                maxW={{ base: '100%', sm: '300px' }}
+                maxW={{ base: '100%', sm: '200px' }}
                 src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
                 alt='Caffe Latte'
-            />
+            /> */}
 
             <Stack>
-                <CardBody>
-                    <Text py='2'>
-                        Caffè latte is a coffee beverage of Italian origin made with espresso
-                        and steamed milk.
+                <CardBody fontWeight={'bold'}>
+                    <Text py='2' >
+                    Icona Pop - I Love It (feat. Charli XCX) [OFFICIAL VIDEO]
                     </Text>
                     <Text>Length: 11:23</Text>
                 </CardBody>
 
 
-                <CardFooter>
+                <CardFooter as={Flex} justifyContent={'center'}>
                     <button className='flex items-center justify-center w-full md:w-[40%] text-white py-2 rounded-md bg-black '>
                         <DownloadIcon />
                         <Text>Download</Text>
