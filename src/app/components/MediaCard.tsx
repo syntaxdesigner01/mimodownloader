16b9c4c965392a335a636a94b642d9b070218650
Akpan joseph <syntaxdesigners@gmail.com>
2023-12-10 09:46:56 +0100

    changed you downloader api

--- src/app/components/MediaCard.tsx
+++ src/app/components/MediaCard.tsx
@@ -4,55 +4,47 @@ import { useSelector } from "react-redux";
 
 export default function MediaCard() {
   const data = useSelector((state: any) => state?.data);
+  const status = useSelector((state: any) => state.status);
 
-  const getmin = data?.duration / 60;
-  const getdecimal = String(getmin).split(".")[1];
-
-  const getsec1 = (Number(getdecimal) * 60).toString()[0];
-  const getsec2 = (Number(getdecimal) * 60).toString()[1];
-
-  const min = Math.floor(data?.duration / 60);
-  const sec = `${getsec1}${getsec2}`;
-
-  // file size
-  const filesize = (data?.filesize / 1048576).toFixed(2);
   return (
     <div className="flex flex-col items-center gap-4 lg:gap-2 lg:flex-row lg:items-start pb-6">
-      <div >
-        <img src={data?.thumbnail} alt="" width={300} height={300} className="rounded-lg" />
+      <div>
+        <img
+          src={data?.thumbnail}
+          alt=""
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
       </div>
 
       <div className="px-4 pt-2 lg:pt-4 shadow-lg py-10 ">
-      <div>
-        <p className="font-bold text-center lg:text-lg">{data?.title}</p>
-      </div>
+        <div>
+          <p className="font-bold text-center lg:text-lg">{data?.title}</p>
+        </div>
 
-      <div>
-       <div className="">
-       <p>
-         Duration - {min}:{sec} mins
-        </p>
-        <p>File size - {filesize} mb</p>
-        <p>File Type Mp3</p>
-        
-       </div>
+        <div className="">
+          <div className=" flex justify-between pb-4 lg:pb-10 lg:pt-4  ">
+            <p>Duration - {Math.floor(data?.length / 60)}mins</p>
+            <p>File size - {data?.size}</p>
+            {/* <p className="hidden lg:block">File Type Mp3</p> */}
+          </div>
 
-        <div className=" pt-4 lg:flex justify-end">
-       {
-        data?.link.length > 0 ? ( <button className="bg-black text-white w-full py-3 rounded-xl font-bold lg:w-1/2" >
-        <a href={data.link} download={data?.title}>
-          Download Mp3
-        </a>
-      </button>):
-
-      <p className="text-red-500">Sorry we could not get this audio please try another vidoe</p>
-
-       }
+          <div className=" pt-4 lg:flex justify-end">
+            {status !== 'rejected'? (
+              <button className="bg-black text-white w-full py-3 rounded-xl font-bold lg:w-1/2">
+                <a href={data.link} download={data?.title}>
+                  Download Mp3
+                </a>
+              </button>
+            ) : (
+              <p className="text-red-500">
+                Sorry we could not get this audio please try another vidoe
+              </p>
+            )}
+          </div>
         </div>
       </div>
-      
-      </div>
-     
     </div>
   );
 }
